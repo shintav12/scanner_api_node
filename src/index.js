aabaa57bb3ea0df88d86d33cd5d056a88b24b5ea
Manuel Alzamora <manuel.alzamora@ideamultimedia.net>
2019-03-16 00:37:01 -0500

    fixes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-
 // Settings
 app.set('port', process.env.port || 3000);
 
 // Middlewares
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 // Routes
 app.use(require('./routes/case'));
--- src/routes/car.js
+++ src/routes/car.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const mysqlConnection = require('../database');
 
-router.get('/car/years/:brand_id', (req, res) => {
+router.get('/car/:brand_id', (req, res) => {
     const { brand_id } = req.params;
     mysqlConnection.query('SELECT DISTINCT year FROM car WHERE brand_id = ?', [brand_id], (err, rows, fields) => {
         if (!err) {
@@ -14,7 +14,7 @@ router.get('/car/years/:brand_id', (req, res) => {
     });
 });
 
-router.get('/car/years/:brand_id/:year', (req, res) => {
+router.get('/car/:brand_id/:year', (req, res) => {
     const { brand_id, year } = req.params;
     mysqlConnection.query('SELECT DISTINCT name FROM car WHERE brand_id = ? and year = ?', [brand_id, year], (err, rows, fields) => {
         if (!err) {
@@ -25,7 +25,7 @@ router.get('/car/years/:brand_id/:year', (req, res) => {
     });
 });
 
-router.get('/car/years/:brand_id/:year/:model', (req, res) => {
+router.get('/car/:brand_id/:year/:model', (req, res) => {
     const { brand_id, year, model } = req.params;
     mysqlConnection.query('SELECT DISTINCT engine FROM car WHERE brand_id = ? and year = ? and name = ?', [brand_id, year, model], (err, rows, fields) => {
         if (!err) {
@@ -34,6 +34,19 @@ router.get('/car/years/:brand_id/:year/:model', (req, res) => {
             console.log(err)
         }
     });
+});
+
+router.get('/car/:brand_id/:year/:model/:engine', (req, res) => {
+    const { brand_id, year, model, engine } = req.params;
+    mysqlConnection.query('SELECT c.*, b.name as brand FROM car c ' +
+        'join brand b on c.brand_id = b.id ' +
+        'WHERE c.brand_id = ? and c.year = ? and c.name = ? and c.engine = ?', [brand_id, year, model, engine], (err, rows, fields) => {
+            if (!err) {
+                res.json(rows[0]);
+            } else {
+                console.log(err)
+            }
+        });
 });
 
 module.exports = router;
